a25624c8f9c720909ba242f6a30c51c9a1cf3791
JendareyTechnologies <ak@jendareyconsulting.com>
2024-02-01 02:26:09 +0000

    my first commit

---
+++ src/main/java/com/jendareytech/devopstest/DevopstestApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.devopstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jendareytech/devopstest/DevopstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jendareytech.devopstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
